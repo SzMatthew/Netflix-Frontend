060c95c93847a50bc4106121e8469bb5b4d20791
Máté Szabó <szabomati@gmail.com>
2021-01-19 09:54:04 +0100

    Add Movie Header Component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,82 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Route, Router, Switch, useParams} from 'react-router-dom';
 import { useSearch} from '../../Contexts/search-context'
 import './Header.scss';
 
 const Header = () => { 
     const {setSearch} = useSearch();
+    const [movie, setMovie] = useState(null);
+    const {id} = useParams();
+    
+    useEffect(() => {
+        getMovie();
+     }, []);
+
+    const getMovie = () => { 
+        fetch('http://localhost:4000/movies/' + 383498)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setMovie(result);
+                }
+            );
+    }
+
+    const SearchHeader = () => { 
+        return (
+            <header className="search-header">
+                <div className="top-nav">
+                    <span className="logo"><b>netflix</b>roulette</span>
+                    <button className="add-movie">ADD MOVIE</button>
+                </div>
+                <section className="search-container">
+                    <h2>FIND YOUR MOVIE</h2>
+                    <div className="search-input">
+                        <input type="text" placeholder="What do you want to watch?" onChange={ (event) => setSearch(event.target.value)}/>
+                    </div>
+                </section>
+            </header>
+        );
+    }
+
+    const MovieHeader = () => {
+        if (movie !== null)
+        {
+            return (
+                <header className="movie-header">
+                    <div className="top-nav">
+                        <span className="logo"><b>netflix</b>roulette</span>
+
+                    </div>
+                    <div className="movie-container">
+                        <img src={movie.poster_path} alt={movie.title}></img>
+                        <div className="details">
+                            <div className="title-score-container">
+                                <h2>{movie.title}</h2>
+                                <div className="score">
+                                    <span>{movie.vote_average}</span>
+                                </div>
+                            </div>
+                            <div className="tagline">
+                                <span>{movie.tagline}</span>
+                            </div>
+                            <div className="date-minutes-container">
+                                <span>{movie.release_date.split('-')[0]}</span>
+                                <span className="runtime">{movie.runtime} min</span>
+                            </div>
+                            <div className="overview">
+                                <p>{movie.overview}</p>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            );
+        }
+        return <div></div>;
+     }
 
     return (
-        <header>
-            <div className="top-nav">
-                <span className="logo"><b>netflix</b>roulette</span>
-                <button className="add-movie">ADD MOVIE</button>
-            </div>
-            <section className="search-container">
-                <h2>FIND YOUR MOVIE</h2>
-                <div className="search-input">
-                    <input type="text" placeholder="What do you want to watch?" onChange={ (event) => setSearch(event.target.value)}/>
-                </div>
-            </section>
-        </header>
+        <MovieHeader/>
     );
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useCategory} from '../../Contexts/category-context';
 import {useOrderBy} from '../../Contexts/order-by-context';
-import {Link} from "react-router-dom"
+import {Link, useHistory} from "react-router-dom"
 import './Navbar.scss';
 
 const Navbar = () => { 
@@ -12,14 +12,19 @@ const Navbar = () => {
         setOrderBy(event.target.value);
     }
 
+    const navbarClick = category => { 
+        setCategory(category);
+        //history.push('/' + String(category).toLowerCase());
+    }
+
     return (
         <nav>
             <ul>
-                <li className={category === '' ? 'active' : ''} onClick={() => setCategory('ALL')}><Link to="/">ALL</Link></li>
-                <li className={category === 'DOCUMENTARY' ? 'active' : '' } onClick={() => setCategory('DOCUMENTARY')}><Link to="/documentary">DOCUMENTARY</Link></li>
-                <li className={category === 'COMEDY' ? 'active' : ''} onClick={() => setCategory('COMEDY')}><Link to="/comedy">COMEDY</Link></li>
-                <li className={category === 'HORROR' ? 'active' : ''} onClick={() => setCategory('HORROR')}><Link to="/horror">HORROR</Link></li>
-                <li className={category === 'CRIME' ? 'active' : ''} onClick={() => setCategory('CRIME')}><Link to="/crime">CRIME</Link></li>
+                <li className={category === '' ? 'active' : ''} onClick={navbarClick.bind(this,'ALL')}><Link to="/">ALL</Link></li>
+                <li className={category === 'DOCUMENTARY' ? 'active' : '' } onClick={navbarClick.bind(this, 'DOCUMENTARY')}><Link to="/documentary">DOCUMENTARY</Link></li>
+                <li className={category === 'COMEDY' ? 'active' : ''} onClick={navbarClick.bind(this, 'COMEDY')}><Link to="/comedy">COMEDY</Link></li>
+                <li className={category === 'HORROR' ? 'active' : ''} onClick={navbarClick.bind(this, 'HORROR')}><Link to="/horror">HORROR</Link></li>
+                <li className={category === 'CRIME' ? 'active' : ''} onClick={navbarClick.bind(this, 'CRIME')}><Link to="/crime">CRIME</Link></li>
             </ul>
             <ul>
                 <li>
